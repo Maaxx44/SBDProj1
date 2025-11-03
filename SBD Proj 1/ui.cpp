ef0358aa7afe250d5d8d076399fb36cbc7e2d110
Maks <Maks@MAKS>
2025-11-04 00:49:00 +0100

    Finished first prototype of drawTable [F]

--- SBD Proj 1/ui.cpp
+++ SBD Proj 1/ui.cpp
@@ -26,19 +26,24 @@ void ui::drawTable(uiTable& table) const {
 	printf("%c", table.getTitle()[14]);
 
 	// Writing content
+	std::vector<std::string> drawContent = table.getContent(0);
 	for (unsigned int i = 0; i < contentBox.H; i++) {
 		this->cInfo.setCursorPosition(contentBox.X, contentBox.Y + i);
 
-		// Upper barrier
+		// Upper and lower barier
+		WORD modifiers = NULL;
+		if (i == 0)					modifiers |= COMMON_LVB_GRID_HORIZONTAL;
+		if (i == contentBox.H - 1)	modifiers |= COMMON_LVB_UNDERSCORE;
 
-		// Lower barrier
+		// Writing Line
+		this->changeColor(table.contentColor | modifiers | COMMON_LVB_GRID_LVERTICAL);
+		printf("%c", drawContent[i][0]);
+		this->changeColor(table.contentColor | modifiers);
+		printf("%s", drawContent[i].substr(1, titleBox.W - 2).c_str());
+		this->changeColor(table.contentColor | modifiers | COMMON_LVB_GRID_RVERTICAL);
+		printf("%c", drawContent[i][14]);
+
 	}
-
-
-	this->cInfo.setCursorPosition(titleBox.X, titleBox.Y + 1);
-	this->changeColor(table.contentColor | COMMON_LVB_GRID_HORIZONTAL);
-	printf("               ");
-
 }
 
 
@@ -46,27 +51,8 @@ void ui::drawTable(uiTable& table) const {
 ui::ui() {
 	this->cInfo.setConsoleTitle("SBD Proj 1");
 	this->initConsole();
-	/*
-	this->changeColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY | COMMON_LVB_UNDERSCORE);
-	printf("      \n");
-	this->changeColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY | COMMON_LVB_GRID_RVERTICAL);
-	printf(" ");
-	this->changeColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-	printf("what");
-	this->changeColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY | COMMON_LVB_GRID_LVERTICAL);
-	printf(" \n");
-	this->changeColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY | COMMON_LVB_GRID_HORIZONTAL);
-	printf("      \n");
-	this->resetColor();
-	*/
-
 
 	//this->cInfo.setConsoleSize(150, 40);
-	//for (int j = 0; j <= 36; j++) {
-	//	for (int i = 0; i <= 150; i++) {
-	//		printf("*");
-	//	}
-	//}
 
 	uiTable testTable = uiTable({ .X = 2, .Y = 1, .W = 15, .H = 5 }, "TitleTitle", { "File 1", "File 2", "File 3", "File 4", "File 5"});
 	std::vector<std::string> testFiles = testTable.getContent(2);
@@ -75,7 +61,13 @@ ui::ui() {
 	//for (unsigned int i = 0; i < testFiles.size(); i++)
 	//	std::cout << testFiles[i] << "  -  length: " << testFiles[i] .size() << std::endl;
 
-	testTable.titleColor = FOREGROUND_RED | FOREGROUND_INTENSITY;
+	testTable.titleColor = FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+	testTable.contentColor = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY;
+	this->drawTable(testTable);
+
+	testTable.resizeTable({ .X = 17, .Y = 1, .W = 15, .H = 5 });
+	testTable.titleColor = FOREGROUND_RED;
+	testTable.contentColor =  FOREGROUND_BLUE | FOREGROUND_INTENSITY;
 	this->drawTable(testTable);
 
 	//testTable.setPositionToText();
